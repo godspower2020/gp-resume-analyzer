3c98025c64fa93a6d25400444b5e0c3fffb15a21
godspower2020 <augustinegodspower40@gmail.com>
2025-10-28 10:17:54 +0100

    fetch real resumes in the home page

--- app/components/resume/resumeCard.tsx
+++ app/components/resume/resumeCard.tsx
@@ -1,18 +1,39 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Link} from "react-router";
 import ScoreCircle from "~/components/dynamicgauge/ScoreCircle";
+import {usePuterStore} from "~/lib/puter";
 
 const ResumeCard = ({resume}: {resume: Resume}) => {
+    const [resumeUrl, setResumeUrl] = useState('');
+    const { fs } = usePuterStore()
+
+    useEffect(() => {
+        const loadResume = async() => {
+            const blob = await fs.read(resume.imagePath)
+            if(!blob) return;
+
+            let url = URL.createObjectURL(blob);
+            setResumeUrl(url)
+        }
+
+        loadResume()
+    }, [resume.imagePath]);
+
     return (
         <Link to={`/resume/${resume.id}`} className="resume-card animate-in fade-in duration-1000">
             <div className="resume-card-header">
                 <div className="flex flex-col gap-2">
-                    <h2 className="!text-black font-bold break-words">
-                        {resume.companyName}
-                    </h2>
-                    <h3 className="text-lg text-gray-500 break-words">
-                        {resume.jobTitle}
-                    </h3>
+                    {resume.companyName && (
+                        <h2 className="!text-black font-bold break-words">
+                            {resume.companyName}
+                        </h2>
+                    )}
+                    {resume.jobTitle && (
+                        <h3 className="text-lg text-gray-500 break-words">
+                            {resume.jobTitle}
+                        </h3>
+                    )}
+                    {(!resume.companyName && !resume.jobTitle) && <h2 className="!text-black font-bold">Resume</h2>}
                 </div>
 
                 <div className="flex-shrink-0">
@@ -20,15 +41,17 @@ const ResumeCard = ({resume}: {resume: Resume}) => {
                 </div>
             </div>
 
-            <div className="gradient-border animate-in fade-in duration-1000">
-                <div className="w-full h-full">
-                    <img
-                        src={resume.imagePath}
-                        alt="resume"
-                        className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
-                    />
+            {resumeUrl && (
+                <div className="gradient-border animate-in fade-in duration-1000">
+                    <div className="w-full h-full">
+                        <img
+                            src={resumeUrl}
+                            alt="resume"
+                            className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
+                        />
+                    </div>
                 </div>
-            </div>
+            )}
         </Link>
     )
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,10 @@
-import {useEffect} from "react";
-import {useLocation, useNavigate} from "react-router";
+import {useEffect, useState} from "react";
+import {Link, useNavigate} from "react-router";
 
 import Navbar from "~/components/navbar/Navbar";
 import ResumeCard from "~/components/resume/resumeCard";
 
 import type { Route } from "./+types/home";
-
-import {resumes} from "../../models";
 
 import {usePuterStore} from "~/lib/puter";
 
@@ -19,11 +17,31 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
     const navigate = useNavigate()
-    const { auth } = usePuterStore()
+    const [resumes, setResumes] = useState<Resume[]>([]);
+    const [loadingResumes, setLoadingResumes] = useState(false);
+    const { auth, kv } = usePuterStore()
 
     useEffect(() => {
         if(!auth.isAuthenticated) navigate('/auth?next=/')
     }, [auth.isAuthenticated]);
+
+    useEffect(() => {
+        const loadResumes = async() => {
+            setLoadingResumes(true);
+
+            const resumes = (await kv.list('resume:*', true)) as KVItem[]
+
+            const parsedResumes = resumes?.map((resume) => (
+                JSON.parse(resume.value) as Resume
+            ))
+
+            console.log('parsedResumes:', parsedResumes);
+            setResumes(parsedResumes || []);
+            setLoadingResumes(false);
+        }
+
+        loadResumes()
+    }, [])
 
   return (
       <main className="bg-[url('/images/bg-main.svg')] bg-cover">
@@ -32,14 +50,31 @@ export default function Home() {
         <section className="main-section">
             <div className="page-heading py-16">
                 <h1>Track Your Applications & Resume Ratings</h1>
-                <h2>Review your submissions and check AI-powered feedback.</h2>
+                {!loadingResumes && resumes?.length === 0 ? (
+                    <h2>No resumes found. Upload your first resume to get feedback.</h2>
+                ) : (
+                    <h2>Review your submissions and check AI-powered feedback.</h2>
+                )}
             </div>
 
-            {resumes.length > 0 && (
+            {loadingResumes && (
+                <div className="flex flex-col items-center justify-center">
+                    <img src="/images/resume-scan-2.gif" alt="resume loading image" className="w-[200px]" />
+                </div>
+            )}
+            {(!loadingResumes && resumes.length > 0) && (
                 <div className="resumes-section">
                     {resumes.map((resume) => (
                         <ResumeCard key={resume.id} resume={resume} />
                     ))}
+                </div>
+            )}
+
+            {!loadingResumes && resumes?.length === 0 && (
+                <div className="flex flex-col items-center justify-center mt-10 gap-4">
+                    <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
+                        Upload Resume
+                    </Link>
                 </div>
             )}
         </section>
