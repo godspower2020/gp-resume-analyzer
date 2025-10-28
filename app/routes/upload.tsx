cecd49d1ea5d307bd9119b4bb6b5109cb83938fa
godspower2020 <augustinegodspower40@gmail.com>
2025-10-28 06:48:04 +0100

    implemented resume feedback page

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -83,6 +83,8 @@ const Upload = () => {
         setStatusText("Analysis complete, redirecting...");
 
         console.log(data)
+
+        navigate(`/resume/${uuid}`)
     }
 
     const handleSubmit = (e:FormEvent<HTMLFormElement>) => {
